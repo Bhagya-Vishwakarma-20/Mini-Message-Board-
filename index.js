8a97937f064e20c36fb6aa7ba0266bca7bd0a404
Bhagya <163407627+Bhagya-Vishwakarma-20@users.noreply.github.com>
2024-12-06 00:48:12 +0530

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const indexRoute = require('./routes/index')
+require('dotenv').config() 
 
 const app = express();
 
@@ -12,4 +13,5 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/',indexRoute)
   
-app.listen(3030)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
